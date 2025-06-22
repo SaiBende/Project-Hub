7f606e691b9ac82eda305a61378268b1f64f6d22
SaiBende <bendesai5703@gmail.com>
2025-06-22 12:09:12 +0530

    fix: update session path formatting and change TypeScript module to ES2015

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -31,7 +31,7 @@ app.use(
   session({
     name: "session",
     keys: [config.SESSION_SECRET],
-    path: "/",
+    path:"/",
     maxAge: 24 * 60 * 60 * 1000,
     secure: config.NODE_ENV === "production",
     httpOnly: true,
